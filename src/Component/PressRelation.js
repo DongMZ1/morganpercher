94a506ce1b15440ebbe8fdb084c90f3577322582
DongMZ1 <915094594@qq.com>
2021-05-27 17:17:24 -0400

    add image display

--- src/Component/PressRelation.js
+++ src/Component/PressRelation.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import '../SCSS/main.scss'
+import testerimage from '../pictures/tester.jpg'
 
 const PressRelation = () =>{
     return <div className='pressrelation' id='pressrelation' data-aos="fade-down-left">
-          press relation part
+           <div className='pressrelation_image_container'>
+              <img src={testerimage} />
+              <img src={testerimage} />
+              <img src={testerimage} />
+              <img src={testerimage} />
+              <img src={testerimage} />
+              <img src={testerimage} />
+              </div>   
+               
     </div>
 }
 export default PressRelation;
--- src/Component/Professional.js
+++ src/Component/Professional.js
@@ -3,7 +3,7 @@ import '../SCSS/main.scss'
 
 const Professtional = () =>{
     return <div className='prefessional' id='prefessional' data-aos="fade-down-right">
-          prefesstional part
+          
     </div>
 }
 export default Professtional;
