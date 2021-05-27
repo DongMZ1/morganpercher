ac0fccd09b8c2f5d83e2b8f6ad704fc1ab39bf56
DongMZ1 <915094594@qq.com>
2021-05-27 01:15:59 -0400

    video done

--- src/Component/Presentation.js
+++ src/Component/Presentation.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../SCSS/main.scss'
 import video from '../Video/tester.mp4'
 
 const Presentation = () =>{
+    const [playvideo, setplayvideo] = useState(true);
+    const playorparsevideo = () =>{
+        const v = document.getElementById('firstvideo');
+            if(playvideo === true){
+            v.pause();
+            setplayvideo(false);
+            }else{
+                v.play();
+                setplayvideo(true);
+            }
+    }
+
     return <div className='presentation' id='presentation' data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-        
-             <video autoPlay loop style={{width: '100%', position:'absolute'}}>
-                 <source src={video} type='video/mp4'></source>
+         
+        <div onClick={playorparsevideo} className='presentation_video_container'>
+             <video autoPlay loop width='100%' id='firstvideo'>
+                 <source src={video} type='video/mp4' />
              </video>
+             <div className='presentation_videocontenttop'>
+                 hello 
+              </div>
+              </div>
+                 
+ 
         
     </div>;
 }
