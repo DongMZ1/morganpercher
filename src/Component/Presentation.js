22b8c34aa502e5a229dcaad67ae7be4ca7dbc04a
DongMZ1 <915094594@qq.com>
2021-05-26 22:27:07 -0400

    fixed bug

--- src/Component/Presentation.js
+++ src/Component/Presentation.js
@@ -5,7 +5,7 @@ import video from '../Video/tester.mp4'
 const Presentation = () =>{
     return <div className='presentation' id='presentation' data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
         
-             <video autoPlay loop>
+             <video autoPlay loop style={{width: '100%', position:'absolute'}}>
                  <source src={video} type='video/mp4'></source>
              </video>
         
