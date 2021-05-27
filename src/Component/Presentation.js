39cfd67f2f68a496ae5dbc7060882c10ec8ae958
DongMZ1 <915094594@qq.com>
2021-05-26 22:13:53 -0400

    find the bug

--- src/Component/Presentation.js
+++ src/Component/Presentation.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../SCSS/main.scss'
+import video from '../Video/tester.mp4'
 
 const Presentation = () =>{
-    return <div className='presentation' id='presentation' data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
-             presentation part
-    </div>
+    return (<> </>);
+    <div className='presentation' id='presentation' data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+        
+             <video autoPlay loop>
+                 <source src={video} type='video/mp4'></source>
+             </video>
+        
+    </div>;
 }
 export default Presentation;
