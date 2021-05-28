025903e1e3e1bafc54274f603e3de4e44b7e07d6
DongMZ1 <915094594@qq.com>
2021-05-27 20:09:06 -0400

    fix cursor issue

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import '../SCSS/main.scss'
+import React from "react";
+import "../SCSS/main.scss";
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
-const Footer = () =>{
-    return<div className='footer' data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
-       <h7>morgan.percher@gmail.com</h7>
-       <h7>514-231-3140</h7>
-       <h7>Montréal</h7>
-       <h7>©2021 par Morgan Percher</h7>
+const Footer = () => {
+  return (
+    <div
+      className="footer"
+      data-aos="fade-down"
+      data-aos-easing="linear"
+      data-aos-duration="1500"
+    >
+      <h7>
+        morgan.percher@gmail.com
+        <a href="https://www.linkedin.com/in/morgan-percher-b78190170/">
+          <AiOutlineLinkedin style={{fontSize: '35px'}} />
+        </a>
+      </h7>
+      <h7>514-231-3140</h7>
+      <h7>Montréal</h7>
+      <h7>©2021 par Morgan Percher</h7>
     </div>
-}
+  );
+};
 
 export default Footer;
