fd831122c9a9d2a835460cc205a181f791a85489
DongMZ1 <915094594@qq.com>
2021-05-27 20:39:41 -0400

    rename prefesstional.scss to professtional.scss

--- src/Component/Professional.js
+++ src/Component/Professional.js
@@ -3,6 +3,7 @@ import '../SCSS/main.scss'
 
 const Professtional = () =>{
     return <div className='prefessional' id='prefessional' data-aos="fade-down-right">
+        professtional page
           
     </div>
 }
