65386722e44a8b29b0b78042fbbd1593f1c00362
DongMZ1 <915094594@qq.com>
2021-05-30 14:31:57 -0400

    doing pressrelation

--- src/Component/PressRelation.js
+++ src/Component/PressRelation.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../SCSS/main.scss";
 import testerimage from "../pictures/tester.jpg";
 import {useTranslation} from 'react-i18next'
+import item1 from '../pressrelation/item1.webp'
 
 const PressRelation = () => {
     const [translate, i18n] = useTranslation();
@@ -13,13 +14,25 @@ const PressRelation = () => {
       <div className="headermiddle">Présentation de l'ensemble des communiqués et dossiers de presse que
           j'ai réalisé.</div>
       <div className="pressrelation_grid_container">
-        <div className='regularcol'>hello</div>
-        <div className='regularcol'>hello</div>
-        <div className='regularcol'>hello</div>
-        <div className='regularcol'>hello</div>
-        <div className='regularcol'>hello</div>
-        <div className='regularcol'>hello</div>
-        <div className='regularcol'>hello</div>
+        <div className='regularcol'>
+            <div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <img src={item1} />
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p>
+            
+            </div>
+            {/**--------------------------------------------------------------------------- */}
+        <div className='regularcol'><div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p></div>
+            <div className='regularcol'><div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p></div>
+            <div className='regularcol'><div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p></div>
+            <div className='regularcol'><div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p></div>
+            <div className='regularcol'><div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p></div>
+            <div className='regularcol'><div className='regularcol_header'>CROISIC OCEARIUM</div>
+            <p>A loggerhead turtle found washed up on a beach in Sarzeau</p></div>
       </div>
     </div>
   );
