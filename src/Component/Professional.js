500195b5fe460474cb5d3e1a983af3ff65a05342
DongMZ1 <915094594@qq.com>
2021-06-07 21:38:16 -0400

    finish entire website

--- src/Component/Professional.js
+++ src/Component/Professional.js
@@ -13,88 +13,172 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h3 data-aos="fade-right"><b>{translate("exper.job")}</b></h3>
-      <h3 data-aos="fade-right"><b>{translate("exper.job")}</b></h3>
-      <h3 data-aos="fade-right"><b>{translate("exper.job")}</b></h3>
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item1a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item1b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item1c")}</h4>
     </div>
-    <div class="body" data-aos="fade-left">
-      <ul>
-        <li><b>{translate("edu.school")}</b>{translate("edu.item1a")}</li>
-        <li><b>{translate("edu.degree")}</b>{translate("edu.item1b")}</li>
-        <li><b>{translate("edu.study")}</b>{translate("edu.item1c")}</li>
-        <li><b>{translate("edu.result")}</b>{translate("edu.item1d")}</li>
-      </ul>
+    <div class="body">
+      <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item1d")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item1e")}</p>
+      <p data-aos="fade-left"><b>💭 Communication : </b>{translate("exper.item1f")}</p>
     </div>
   </div>
 
  <div class="entry">
     <div class="title">
-      <h3 data-aos="fade-right">2013 - 2015</h3>
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item2a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item2b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item2c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item2d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item2e")}</h4>
     </div>
-    <div class="body" data-aos="fade-left">
-      <ul>
-        <li><b>{translate("edu.school")}</b>{translate("edu.item2a")}</li>
-        <li><b>{translate("edu.degree")}</b>{translate("edu.item2b")}</li>
-        <li><b>{translate("edu.study")}</b>{translate("edu.item2c")}</li>
-        <li><b>{translate("edu.result")}</b>{translate("edu.item2d")}</li>
-      </ul>
-    </div>
-  </div>
-
-  <div class="entry">
-    <div class="title">
-      <h3 data-aos="fade-right">2012 - 2013</h3>
-    </div>
-    <div class="body" data-aos="fade-left">
-      <ul>
-        <li><b>{translate("edu.school")}</b>{translate("edu.item3a")}</li>
-        <li><b>{translate("edu.degree")}</b>{translate("edu.item3b")}</li>
-        <li><b>{translate("edu.study")}</b>{translate("edu.item3c")}</li>
-        <li><b>{translate("edu.result")}</b>{translate("edu.item3d")}</li>
-      </ul>
-    </div>
-  </div>
-
-
-  <div class="entry">
-    <div class="title">
-      <h3 data-aos="fade-right">2011 - 2012</h3>
-    </div>
-    <div class="body" data-aos="fade-left">
-      <ul>
-        <li><b>{translate("edu.school")}</b>{translate("edu.item4a")}</li>
-        <li><b>{translate("edu.degree")}</b>{translate("edu.ite4b")}</li>
-        <li><b>{translate("edu.study")}</b>{translate("edu.item4c")}</li>
-        <li><b>{translate("edu.result")}</b>{translate("edu.item4d")}</li>
-      </ul>
+    <div class="body">
+      <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item2f")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item2g")}</p>
+      <p data-aos="fade-left"><b>💭 Communication : </b>{translate("exper.item2h")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.creation")}</b>{translate("exper.item2i")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.pressrelation")}</b>{translate("exper.item2j")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.web")}</b>{translate("exper.item2k")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.ecology")}</b>{translate("exper.item2l")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.eventandex")}</b>{translate("exper.item2m")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.sideproject1")}</b>{translate("exper.item2n")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.sideproject2")}</b>{translate("exper.item2o")}</p>
     </div>
   </div>
 
   <div class="entry">
     <div class="title">
-      <h3 data-aos="fade-right">2011</h3>
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item3a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item3b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item3c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item3d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item3e")}</h4>
     </div>
-    <div class="body" data-aos="fade-left">
-      <ul>
-        <li><b>{translate("edu.school")}</b>{translate("edu.item5a")}</li>
-        <li><b>{translate("edu.degree")}</b>{translate("edu.item5b")}</li>
-        <li><b>{translate("edu.study")}</b>{translate("edu.item5c")}</li>
-        <li><b>{translate("edu.result")}</b>{translate("edu.item5d")}</li>
-      </ul>
+    <div class="body">
+      <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item3f")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item3g")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.event")}</b>{translate("exper.item3h")}</p>
+      <p data-aos="fade-left"><b>💭 Communication : </b>{translate("exper.item3i")}</p>
+      <p data-aos="fade-left"><b>{translate("exper.creation")}</b>{translate("exper.item3j")}</p>
+    </div>
+  </div>
+
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item4a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item4b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item4c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item4d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item4e")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item4f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item4g")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.pressrelation")}</b>{translate("exper.item4h")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.creation")}</b>{translate("exper.item4i")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.web")}</b>{translate("exper.item4j")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.eventandex")}</b>{translate("exper.item4k")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.crisis")}</b>{translate("exper.item4l")}</p>
     </div>
   </div>
 
   <div class="entry">
     <div class="title">
-      <h3 data-aos="fade-right">2011</h3>
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item5a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item5b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item5c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item5d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item5e")}</h4>
     </div>
-    <div class="body" data-aos="fade-left">
-      <ul>
-        <li><b>{translate("edu.school")}</b>{translate("edu.item6a")}</li>
-        <li><b>{translate("edu.degree")}</b>{translate("edu.item6b")}</li>
-        <li><b>{translate("edu.study")}</b>{translate("edu.item6c")}</li>
-        <li><b>{translate("edu.result")}</b>{translate("edu.item6d")}</li>
-      </ul>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item5f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item5g")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.creation")}</b>{translate("exper.item5h")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.journal")}</b>{translate("exper.item5i")}</p>
+    </div>
+  </div>
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item6a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item6b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item6c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item6d")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item6e")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item6f")}</p>
+    </div>
+  </div>
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item7a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item7b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item7c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item7d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item7e")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item7f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item7g")}</p>
+    </div>
+  </div>
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item8a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item8b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item8c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item8d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item8e")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item8f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item8g")}</p>
+    </div>
+  </div>
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item9a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item9b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item9c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item9d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item9e")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item9f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item9g")}</p>
+    </div>
+  </div>
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item10a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item10b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item10c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item10d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item10e")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item10f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item10g")}</p>
+    </div>
+  </div>
+
+  <div class="entry">
+    <div class="title">
+      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item11a")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item11b")}</h4>
+      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item11c")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item11d")}</h4>
+      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item11e")}</h4>
+    </div>
+    <div class="body">
+        <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item11f")}</p>
+        <p data-aos="fade-left"><b>{translate("exper.function")}</b>{translate("exper.item11g")}</p>
     </div>
   </div>
 
