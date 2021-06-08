1b6745ca3fdf1cffc6b2811aad935aa8f8faec9d
DongMZ1 <915094594@qq.com>
2021-06-07 21:47:34 -0400

    style fixing on mobile

--- src/Component/Professional.js
+++ src/Component/Professional.js
@@ -13,9 +13,9 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item1a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item1b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item1c")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item1a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item1b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item1c")}</h5>
     </div>
     <div class="body">
       <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item1d")}</p>
@@ -26,11 +26,11 @@ const Professtional = () =>{
 
  <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item2a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item2b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item2c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item2d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item2e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item2a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item2b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item2c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item2d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item2e")}</h5>
     </div>
     <div class="body">
       <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item2f")}</p>
@@ -48,11 +48,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item3a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item3b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item3c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item3d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item3e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item3a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item3b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item3c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item3d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item3e")}</h5>
     </div>
     <div class="body">
       <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item3f")}</p>
@@ -66,11 +66,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item4a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item4b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item4c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item4d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item4e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item4a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item4b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item4c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item4d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item4e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item4f")}</p>
@@ -85,11 +85,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item5a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item5b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item5c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item5d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item5e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item5a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item5b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item5c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item5d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item5e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item5f")}</p>
@@ -101,10 +101,10 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item6a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item6b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item6c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item6d")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item6a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item6b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item6c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item6d")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item6e")}</p>
@@ -114,11 +114,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item7a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item7b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item7c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item7d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item7e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item7a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item7b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item7c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item7d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item7e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item7f")}</p>
@@ -128,11 +128,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item8a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item8b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item8c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item8d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item8e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item8a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item8b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item8c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item8d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item8e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item8f")}</p>
@@ -142,11 +142,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item9a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item9b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item9c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item9d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item9e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item9a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item9b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item9c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item9d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item9e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item9f")}</p>
@@ -156,11 +156,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item10a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item10b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item10c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item10d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item10e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item10a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item10b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item10c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item10d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item10e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item10f")}</p>
@@ -170,11 +170,11 @@ const Professtional = () =>{
 
   <div class="entry">
     <div class="title">
-      <h4 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item11a")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item11b")}</h4>
-      <h4 data-aos="fade-right"><b>Dates : </b>{translate("exper.item11c")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item11d")}</h4>
-      <h4 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item11e")}</h4>
+      <h5 data-aos="fade-right"><b>{translate("exper.job")}</b>{translate("exper.item11a")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.company")}</b>{translate("exper.item11b")}</h5>
+      <h5 data-aos="fade-right"><b>Dates : </b>{translate("exper.item11c")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.duration")}</b>{translate("exper.item11d")}</h5>
+      <h5 data-aos="fade-right"><b>{translate("exper.location")}</b>{translate("exper.item11e")}</h5>
     </div>
     <div class="body">
         <p data-aos="fade-left"><b>{translate("exper.description")}</b>{translate("exper.item11f")}</p>
